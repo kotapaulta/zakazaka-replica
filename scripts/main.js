144da5fa5e902bda9d0784a0153bcf24943a8603
Pavel Smagin <kotapaulta@mail.ru>
2014-11-05 01:46:58 +0600

    add carousel

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
+/*global $*/
+
+$(document).ready(function(){
+  'use strict';
+
+  if ($(".carousel-stock").length) {
+    var b = $(".all-stock").bxSlider({
+      mode: "horizontal",
+      pagerSelector: ".cpager-1",
+      pager: !$(".location-stock").length,
+      useCSS: false,
+      moveSlides: 1,
+      minSlides: 4,
+      maxSlides: 4,
+      infiniteLoop: false,
+      slideWidth: 220,
+      slideMargin: 11,
+      hideControlOnEnd: true,
+      speed: 400
+    });
+  }
+});
+
 
